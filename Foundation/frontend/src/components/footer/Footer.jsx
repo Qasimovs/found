86bf3ef10a316278a3573eb4be67b1ad06b73456
MuradCavadov7 <muradnc-ab108@code.edu.az>
2025-05-14 13:27:52 +0400

    task elave edildi

--- Foundation/frontend/src/components/footer/Footer.jsx
+++ Foundation/frontend/src/components/footer/Footer.jsx
@@ -2,7 +2,31 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='container'>
+      <div className="row">
+        <div className="col-4">
+          <div>
+            <h3>About Us</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis illum sapiente harum debitis? Dolore, obcaecati.
+            </p>
+          </div>
+
+        </div>
+        <div className="col-4">
+          <div>
+            <h3>Features</h3>
+          </div>
+
+        </div>
+        <div className="col-4">
+          <div>
+            <h3>Some PARAQRAf</h3>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
   )
 }
 
